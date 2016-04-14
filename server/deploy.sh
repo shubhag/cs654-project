79cd754f5b52726b04d033553abb35d5920b4c90
Lucky Sahani <luckythegreat4@gmail.com>
2016-04-15 01:51:30 +0530

    Working on script

--- server/deploy.sh
+++ server/deploy.sh
@@ -1,21 +1,13 @@
 #!/bin/bash
 
-cd /public/uploads/
-for file in "$PATH_TO_SOMEWHERE"; 
-do
-	if [ -d $file ]
-	then
-	    # do something directory-ish
-	else
-		if [[ $file == code.zip ]]      
-		then
-		    ls
-		fi
-	fi
-done;
-unzip code.zip
-# rm -rf docker
-# mkdir docker
-# cd docker
-# cerate new folder here
-# unzip
+rm -rf docker
+mkdir docker
+unzip public/uploads/code -d docker/
+cp -a files/. docker/
+cd docker
+mv nodejs-dockerfile dockerfile
+
+
+# do something
+
+rm dockerfile
