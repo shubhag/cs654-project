9635ae19681b81d128ddcc377f4b9e456ecfc85c
Shubham Agrawal <shubhamagrawal094@gmail.com>
2016-04-14 23:32:41 +0530

    replica done

---
+++ Project/docker-install.sh
@@ -0,0 +1,7 @@
+sudo apt-get update
+sudo apt-get -y upgrade
+sudo apt-get install -y linux-image-extra-`uname -r`
+sudo apt-key adv --keyserver hkp://pgp.mit.edu:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
+echo "deb https://apt.dockerproject.org/repo ubuntu-trusty main" | sudo tee /etc/apt/sources.list.d/docker.list
+sudo apt-get update
+sudo apt-get install -y docker-engine
