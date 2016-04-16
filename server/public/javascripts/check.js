67e850e3d072bbd6ca738e12dae7f30ebd1006a5
Lucky Sahani <luckythegreat4@gmail.com>
2016-04-16 15:53:37 +0530

    Pulling bash scripts

--- server/public/javascripts/check.js
+++ server/public/javascripts/check.js
@@ -1,5 +1,11 @@
- $("#docker_details_form").on("submit", function(){
-	if ($('#fileUpload').get(0).files.length === 0) {
-	    console.log("No files selected.");
+$(document).ready(function(){
+	$("form").submit(function(){
+	var fileName = $("#fileUpload").val();
+	if(fileName) { // returns true if the string is not empty
+	    alert(fileName + " was selected");
+	} else { // no file was selected
+	    alert("no file selected");
+	    return false; //<---- Add this line.
 	}
-})
+	});
+});
